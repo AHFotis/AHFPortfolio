7e8b9d8caef90d7535c88647ceea9c23b6c2da8c
Anna Fotis <annahickeyfotis@gmail.com>
2023-03-16 14:17:35 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ahfportfolio/precache-manifest.c72967b5d226a57aa769165c7a6140ae.js"
+  "/AHFPortfolio/precache-manifest.29ec610fee206d6cfbcbd9a27cba0cfb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ahfportfolio/index.html", {
+workbox.routing.registerNavigationRoute("/AHFPortfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
