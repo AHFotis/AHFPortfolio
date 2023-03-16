6c1234e68c1eb2df05c8dd8564fc585824de47ce
Anna Fotis <annahickeyfotis@gmail.com>
2023-03-16 13:20:06 -0500

    Updates

---
+++ precache-manifest.fc49ceeacaa321bde14ed47f07f18272.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "458b4aefa39c6f66e4cf",
+    "url": "/static/css/main.605d5234.chunk.css"
+  },
+  {
+    "revision": "458b4aefa39c6f66e4cf",
+    "url": "/static/js/main.458b4aef.chunk.js"
+  },
+  {
+    "revision": "499bf8bcb9159cbc401f",
+    "url": "/static/js/1.499bf8bc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c3427965c646a7d3ae44e4b47d4a412",
+    "url": "/static/media/AnnaHickeyFotisResume.2c342796.pdf"
+  },
+  {
+    "revision": "7a6938ee528412733071212ac11565c0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AHFPortfolio/precache-manifest.29ec610fee206d6cfbcbd9a27cba0cfb.js"
+  "/precache-manifest.fc49ceeacaa321bde14ed47f07f18272.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/AHFPortfolio/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
